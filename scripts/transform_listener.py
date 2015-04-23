5b46ef8363aaff0711289b0ff2d3707744bcb714
srli <sophia.li@students.olin.edu>
2015-04-23 11:15:56 -0400

    fixed usb_cam to world transform

--- scripts/transform_listener.py
+++ scripts/transform_listener.py
@@ -14,18 +14,28 @@ if __name__ == '__main__':
     turtle_vel = rospy.Publisher('camera_position', geometry_msgs.msg.Twist,queue_size=1)
 
     rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        try:
-            (trans,rot) = listener.lookupTransform('/ar_pose_marker', '/usb_cam', rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            print "exception found"
-            continue
 
-        angular = 4 * math.atan2(trans[1], trans[0])
-        linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-        cmd = geometry_msgs.msg.Twist()
-        cmd.linear.x = linear
-        cmd.angular.z = angular
-        turtle_vel.publish(cmd)
+    transform_dict = {}
+
+    while not rospy.is_shutdown():
+        for i in range(1,2):
+            try:
+                (trans,rot) = listener.lookupTransform('/ar_marker_'+str(i), '/world', rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue                 #if tag not found, we move on to next tag
+            print rot
+            # br = tf.TransformBroadcaster()
+            # br.sendTransform((trans[0], trans[1], 0),
+            #          tf.transformations.quaternion_from_euler(0, 0, rot[0]),
+            #          rospy.Time.now(),
+            #          '/ar_marker_' + str(i),
+            #          'world')
+
+            #angular = 4 * math.atan2(trans[1], trans[0])
+            #linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            # cmd = geometry_msgs.msg.Twist()
+            # cmd.linear = trans
+            # cmd.angular = rot
+            # turtle_vel.publish(cmd)
 
         rate.sleep()
