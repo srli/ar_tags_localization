5adc62c159d6054b091c526730b83524658b8077
srli <sophia.li@students.olin.edu>
2015-04-24 16:55:49 -0400

    starting quaternion math

--- scripts/transform_listener.py
+++ scripts/transform_listener.py
@@ -6,6 +6,7 @@ import math
 import tf
 from std_msgs.msg import Int16MultiArray
 import geometry_msgs.msg
+from tf.transformations import euler_from_quaternion
 import ar_track_alvar_msgs.msg
 
 def callback(data):
@@ -15,12 +16,18 @@ def callback(data):
     y_dists = []
 
     #Manually populate transform dictionary here
-    transform_dict = {2:(0, 1, "up"), 3:(1,0, "right")}
+    transform_dict = {1:(0.3,1.6,"up"), 2:(1.25,0 , "left"), 3:(0.6,0, "right")}
 
     for i in range(len(data.markers)):
+        angles = []
         marker_id = data.markers[i].id
         marker_position = data.markers[i].pose.pose.position #z is our real world x distance, x is real world y.
-        found_markers.append((marker_id, marker_position))
+        marker_orientation = data.markers[i].pose.pose.orientation
+        #print marker_orientation
+        euler_marker = euler_from_quaternion((marker_orientation.x, marker_orientation.y, marker_orientation.z, marker_orientation.w))
+        yaw = euler_marker[1]*(180.0/math.pi)
+        print 'rotation: ', yaw
+        found_markers.append((marker_id, marker_position, yaw))
 
     for marker in found_markers:
         try:
@@ -30,9 +37,12 @@ def callback(data):
         if dict_entry[2] == "up":
             x_dist = marker[1].x - dict_entry[0]
             y_dist = marker[1].z - dict_entry[1]
-        elif dict_entry[2] == "right":
+        elif dict_entry[2] == "left":
             x_dist = marker[1].z - dict_entry[0]
             y_dist = marker[1].x - dict_entry[1]
+        elif dict_entry[2] == "right":
+            x_dist = dict_entry[0] - marker[1].z
+            y_dist = dict_entry[1] - marker[1].x
         x_dists.append(x_dist)
         y_dists.append(y_dist)
 
@@ -41,7 +51,7 @@ def callback(data):
         msg = Int16MultiArray()
         msg.data = camera_location
         pub.publish(msg)
-        print camera_location
+        #print camera_location
 
     except:
         pass
@@ -54,8 +64,15 @@ if __name__ == '__main__':
 
     pub = rospy.Publisher('camera_location', Int16MultiArray,queue_size=1)
     rospy.Subscriber("ar_pose_marker", ar_track_alvar_msgs.msg.AlvarMarkers, callback)
+    listener = tf.TransformListener()
 
     rate = rospy.Rate(10.0)
 
     while not rospy.is_shutdown():
+        # try:
+        #     (trans,rot) = listener.lookupTransform('/usb_cam', '/ar_marker_1', rospy.Time(0))
+        #     #print rot
+        # except (tf.LookupException, tf.ConnectivityException):
+        #     continue
+
         rate.sleep()
